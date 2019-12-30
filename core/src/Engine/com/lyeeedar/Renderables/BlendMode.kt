35ba704a83c84a6d4ea888c12046ad515ad85ea7
lyeeedar <lyeeedar@gmail.com>
2019-12-30 15:56:01 +0000

    Replace the engine with a submodule of the TileEngine repo

--- core/src/Engine/com/lyeeedar/Renderables/BlendMode.kt
+++
@@ -1,9 +0,0 @@
-package com.lyeeedar
-
-import com.badlogic.gdx.graphics.GL20
-
-enum class BlendMode constructor(val src: Int, val dst: Int)
-{
-	MULTIPLICATIVE(GL20.GL_SRC_ALPHA, GL20.GL_ONE_MINUS_SRC_ALPHA),
-	ADDITIVE(GL20.GL_SRC_ALPHA, GL20.GL_ONE);
-}
