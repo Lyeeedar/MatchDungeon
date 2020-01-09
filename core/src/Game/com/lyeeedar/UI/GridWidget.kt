19c15eb21c8332f0a618935c6426eefc81c0d9fe
Lyeeedar <lyeeedar@gmail.com>
2020-01-09 08:44:52 +0000

    Brighten levels by default

--- core/src/Game/com/lyeeedar/UI/GridWidget.kt
+++ core/src/Game/com/lyeeedar/UI/GridWidget.kt
@@ -236,7 +236,7 @@ class GridWidget(val grid: Grid) : Widget()
 		val yp = this.y + (this.height / 2f) - ((grid.height * tileSize) / 2f)
 		renderY = yp
 
-		renderer.begin(Gdx.app.graphics.deltaTime * GridScreen.instance.timeMultiplier, xp, yp, Colour(0.8f, 0.8f, 0.8f, 1f))
+		renderer.begin(Gdx.app.graphics.deltaTime * GridScreen.instance.timeMultiplier, xp, yp, Colour(1f, 1f, 1f, 1f))
 
 		if (grid.activeAbility == null)
 		{
