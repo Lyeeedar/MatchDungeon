09419e16eaa1d68d5ac4a95f4e1dbdff9d6c59f7
lyeeedar <lyeeedar@gmail.com>
2020-01-30 09:37:06 +0000

    Begin work on own entity system

---
+++ core/src/Game/com/lyeeedar/Components/ComponentType.kt
@@ -0,0 +1,40 @@
+package com.lyeeedar.Components
+
+enum class ComponentType private constructor(val constructor: ()->AbstractComponent)
+{
+	// Engine
+	AdditionalRenderable({ AdditionalRenderableComponent() }),
+	Dialogue({ DialogueComponent() }),
+	DirectionalSprite({ DirectionalSpriteComponent() }),
+	Event({ EventComponent() }),
+	LoadData({ LoadDataComponent() }),
+	MarkedForDeletion({ MarkedForDeletionComponent() }),
+	MetaRegion({ MetaRegionComponent() }),
+	Name({ NameComponent() }),
+	Occludes({ OccludesComponent() }),
+	Position({ PositionComponent() }),
+	Renderable({ RenderableComponent() }),
+	Transient({ TransientComponent() }),
+
+	// Game
+	AI({ AIComponent() }),
+	Container({ ContainerComponent() }),
+	Damageable({ DamageableComponent() }),
+	EntityArchetype({ EntityArchetypeComponent() }),
+	Healable({ HealableComponent() }),
+	Matchable({ MatchableComponent() }),
+	MonsterEffect({ MonsterEffectComponent() }),
+	OnTurn({ OnTurnComponent() }),
+	OnTurnEffect({ OnTurnEffectComponent() }),
+	OrbSpawner({ OrbSpawnerComponent() }),
+	Sinkable({ SinkableComponent() }),
+	Special({ SpecialComponent() }),
+	Swappable({ SwappableComponent() }),
+	Tutorial({ TutorialComponent() });
+
+	companion object
+	{
+		val Values = ComponentType.values()
+		val Temporary = arrayOf( MarkedForDeletion, Transient )
+	}
+}
